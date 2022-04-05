7295f1b621449f956919e61840e22970a5baaa8e
Funny-Code-d <sosnin_dienis@mail.ru>
2022-04-05 23:28:20 +0700

    Создание базы данных

--- db/__init__.py
+++ db/__init__.py
@@ -1,6 +1,11 @@
-from .tables import token, customers, category, personal_view, settings_view, purchase, details_purchase, debt_person, debtbook, debtbook_history
-# from .tables import customers
+from .tables import token, customers
+from .tables import debt_person, debtbook, debtbook_history
+from .tables import personal_group, personal_category, personal_purchase, detail_personal_purchase, personal_temlates, personal_templates_category
+from .tables import public_group, public_group_users, public_category, public_purchase, detail_public_purchase, public_templates, public_templates_category
+
 from .base import metadata, engine
+
+
 
 metadata.create_all(bind=engine)
 # metadata.drop_all(bind=engine)
--- db/tables.py
+++ db/tables.py
@@ -1,3 +1,4 @@
+from enum import unique
 from sqlalchemy.schema import Sequence
 from sqlalchemy import CheckConstraint, ForeignKey
 import sqlalchemy
@@ -12,10 +13,10 @@ token = sqlalchemy.Table(
     sqlalchemy.Column("access_token", sqlalchemy.String(30), primary_key=True),
     sqlalchemy.Column("refresh_token", sqlalchemy.String(30), primary_key=True),
     sqlalchemy.Column("owner", sqlalchemy.String(50), nullable=False),
-    sqlalchemy.Column("email", sqlalchemy.String(30), nullable=False),
-    sqlalchemy.Column("application", sqlalchemy.String(10)),
+    sqlalchemy.Column("email_owner", sqlalchemy.String(30), nullable=False),
     sqlalchemy.Column("date_create", sqlalchemy.Date, nullable=False),
-    sqlalchemy.Column("date_end_token", sqlalchemy.Date, nullable=False)
+    sqlalchemy.Column("date_refresh", sqlalchemy.Date, nullable=False),
+    sqlalchemy.Column("date_end", sqlalchemy.Date, nullable=False)
 )
 
 customers = sqlalchemy.Table(
@@ -27,57 +28,6 @@ customers = sqlalchemy.Table(
     sqlalchemy.Column("last_name", sqlalchemy.String(20)),
     sqlalchemy.Column("email", sqlalchemy.String(100), unique=True),
     sqlalchemy.Column("telegram_id", sqlalchemy.Integer, unique=True)
-)
-
-category = sqlalchemy.Table(
-    "category",
-    metadata,
-    sqlalchemy.Column("category_id", sqlalchemy.Integer, Sequence("category_id_seq"),
-                      server_default=Sequence("category_id_seq").next_value(), unique=True, nullable=False),
-    sqlalchemy.Column("user_id", sqlalchemy.Integer, sqlalchemy.ForeignKey("customers.user_id"), nullable=False),
-    sqlalchemy.Column("name_category", sqlalchemy.String(30), nullable=False),
-    sqlalchemy.PrimaryKeyConstraint("user_id", "name_category", name='category_pk')
-)
-
-personal_view = sqlalchemy.Table(
-    "personal_view",
-    metadata,
-    sqlalchemy.Column("view_id", sqlalchemy.Integer, Sequence("view_id_seq"),
-                      server_default=Sequence("view_id_seq").next_value(), unique=True, nullable=False),
-    sqlalchemy.Column("user_id", sqlalchemy.Integer, sqlalchemy.ForeignKey("customers.user_id"), nullable=False),
-    sqlalchemy.Column("name_view", sqlalchemy.String(50), nullable=False),
-    sqlalchemy.Column("number_days", sqlalchemy.Integer, nullable=False),
-    sqlalchemy.PrimaryKeyConstraint("user_id", "name_view", name="personal_view_pk")
-)
-
-settings_view = sqlalchemy.Table(
-    "settings_view",
-    metadata,
-    sqlalchemy.Column("view_id", sqlalchemy.Integer, sqlalchemy.ForeignKey("personal_view.view_id"), nullable=False),
-    sqlalchemy.Column("category_id", sqlalchemy.Integer, sqlalchemy.ForeignKey("category.category_id"), nullable=False),
-    sqlalchemy.PrimaryKeyConstraint("view_id", "category_id", name="settings_view_pk")
-)
-
-
-purchase = sqlalchemy.Table(
-    "purchase",
-    metadata,
-    sqlalchemy.Column("purchase_id", sqlalchemy.Integer, unique=True, autoincrement=True),
-    sqlalchemy.Column("user_id", sqlalchemy.Integer, sqlalchemy.ForeignKey("customers.user_id"), nullable=False),
-    sqlalchemy.Column("total_amount", sqlalchemy.Float, nullable=False),
-    sqlalchemy.Column("category", sqlalchemy.Integer, sqlalchemy.ForeignKey("category.category_id"), nullable=False),
-    sqlalchemy.Column("date_purchase", sqlalchemy.Date, nullable=False),
-    sqlalchemy.PrimaryKeyConstraint("purchase_id", "user_id", name="purchase_pk")
-)
-
-details_purchase = sqlalchemy.Table(
-    "details_purchase",
-    metadata,
-    sqlalchemy.Column("details_purchase_id", sqlalchemy.Integer, autoincrement=True, primary_key=True),
-    sqlalchemy.Column("purchase_id", sqlalchemy.Integer, sqlalchemy.ForeignKey("purchase.purchase_id")),
-    sqlalchemy.Column("name_product", sqlalchemy.String(50), nullable=False),
-    sqlalchemy.Column("amount", sqlalchemy.Float, nullable=False),
-    sqlalchemy.Column("quantity", sqlalchemy.Integer),
 )
 
 debt_person = sqlalchemy.Table(
@@ -111,3 +61,128 @@ debtbook_history = sqlalchemy.Table(
     sqlalchemy.PrimaryKeyConstraint("record_id", name="debtbook_history_pk")
 )
 
+# personal tables
+
+personal_group = sqlalchemy.Table(
+    "personal_cost_group",
+    metadata,
+    sqlalchemy.Column("group_id", sqlalchemy.Integer, Sequence("personal_cost_group_seq"), server_default=Sequence("personal_cost_group_seq").next_value(), unique=True),
+    sqlalchemy.Column("user_id", sqlalchemy.Integer, nullable=False),
+    sqlalchemy.Column("name_group", sqlalchemy.Integer, ForeignKey("customers.user_id"), nullable=False),
+    sqlalchemy.Column("description", sqlalchemy.Text, nullable=True),
+    sqlalchemy.PrimaryKeyConstraint("user_id", "name_group", name="personal_cost_group_pk")
+)
+
+personal_category = sqlalchemy.Table(
+    "personal_category",
+    metadata,
+    sqlalchemy.Column("category_id", sqlalchemy.Integer, Sequence("personal_category_seq"), server_default=Sequence("personal_category_seq").next_value(), unique=True),
+    sqlalchemy.Column("user_id", sqlalchemy.Integer, nullable=False),
+    sqlalchemy.Column("name_category", sqlalchemy.String(30)),
+    sqlalchemy.PrimaryKeyConstraint("user_id", "name_category", name="personal_category_pk")
+)
+
+personal_purchase = sqlalchemy.Table(
+    "personal_purchase",
+    metadata,
+    sqlalchemy.Column("purchase_id", sqlalchemy.Integer, primary_key=True, autoincrement=True),
+    sqlalchemy.Column("group_id", sqlalchemy.Integer, ForeignKey("personal_cost_group.group_id"), nullable=False),
+    sqlalchemy.Column("total_amount", sqlalchemy.Float, nullable=False),
+    sqlalchemy.Column("category_id", sqlalchemy.Integer, ForeignKey("personal_category.category_id")),
+    sqlalchemy.Column("date_registration", sqlalchemy.Date, nullable=False)
+)
+
+detail_personal_purchase = sqlalchemy.Table(
+    "detail_personal_purchase",
+    metadata,
+    sqlalchemy.Column("detail_purchase_id", sqlalchemy.Integer, primary_key=True, autoincrement=True),
+    sqlalchemy.Column("purchase_id", sqlalchemy.Integer, ForeignKey("personal_purchase.purchase_id"), nullable=False),
+    sqlalchemy.Column("name_product", sqlalchemy.String(50), nullable=False),
+    sqlalchemy.Column("price", sqlalchemy.Float, nullable=True),
+    sqlalchemy.Column("amount", sqlalchemy.Float, nullable=False),
+    sqlalchemy.Column("quantity", sqlalchemy.Integer, nullable=True)
+)
+
+personal_temlates = sqlalchemy.Table(
+    "personal_templates",
+    metadata,
+    sqlalchemy.Column("template_id", sqlalchemy.Integer, Sequence("template_id_seq"), server_default=Sequence("template_id_seq").next_value(), unique=True),
+    sqlalchemy.Column("user_id", sqlalchemy.Integer, ForeignKey("customers.user_id"), nullable=False),
+    sqlalchemy.Column("name_template", sqlalchemy.String(50), nullable=False),
+    sqlalchemy.Column("number_days", sqlalchemy.Integer, nullable=False),
+    sqlalchemy.PrimaryKeyConstraint("user_id", "name_template", name="personal_templates_pk")
+)
+
+personal_templates_category = sqlalchemy.Table(
+    "personal_templates_category",
+    metadata,
+    sqlalchemy.Column("template_id", sqlalchemy.Integer, ForeignKey("personal_templates.template_id"), primary_key=True),
+    sqlalchemy.Column("category_id", sqlalchemy.Integer, ForeignKey("personal_category.category_id"), primary_key=True)
+)
+
+# public tables
+
+public_group = sqlalchemy.Table(
+    "public_cost_group",
+    metadata,
+    sqlalchemy.Column("group_id", sqlalchemy.Integer, Sequence("public_cost_group_seq"), server_default=Sequence("public_cost_group_seq").next_value(), unique=True, nullable=False),
+    sqlalchemy.Column("admin_id", sqlalchemy.Integer, nullable=False),
+    sqlalchemy.Column("name_group", sqlalchemy.String(50), nullable=False),
+    sqlalchemy.Column("description", sqlalchemy.Text, nullable=True),
+    sqlalchemy.PrimaryKeyConstraint("admin_id", "name_group", name="public_cost_group_pk")
+)
+
+public_group_users = sqlalchemy.Table(
+    "public_cost_group_users",
+    metadata,
+    sqlalchemy.Column("user_id", sqlalchemy.Integer, ForeignKey("customers.user_id"), primary_key=True),
+    sqlalchemy.Column("group_id", sqlalchemy.Integer, ForeignKey("public_cost_group.group_id"), primary_key=True)
+)
+
+public_category = sqlalchemy.Table(
+    "public_category",
+    metadata,
+    sqlalchemy.Column("category_id", sqlalchemy.Integer, Sequence("public_category_seq"), server_default=Sequence("public_category_seq").next_value(), unique=True),
+    sqlalchemy.Column("group_id", sqlalchemy.Integer, ForeignKey("public_cost_group.group_id")),
+    sqlalchemy.Column("name_category", sqlalchemy.String(30)),
+    sqlalchemy.PrimaryKeyConstraint("group_id", "name_category", name="public_category_pk")
+)
+
+public_purchase = sqlalchemy.Table(
+    "public_purchase",
+    metadata,
+    sqlalchemy.Column("purchase_id", sqlalchemy.Integer, primary_key=True, autoincrement=True),
+    sqlalchemy.Column("group_id", sqlalchemy.Integer, ForeignKey("public_cost_group.group_id"), nullable=False),
+    sqlalchemy.Column("total_amount", sqlalchemy.Float, nullable=False),
+    sqlalchemy.Column("category_id", sqlalchemy.Integer, ForeignKey("public_category.category_id"), nullable=False),
+    sqlalchemy.Column("date_registration", sqlalchemy.Date, nullable=False)
+)
+
+detail_public_purchase = sqlalchemy.Table(
+    "detail_public_purchase",
+    metadata,
+    sqlalchemy.Column("detail_purchase_id", sqlalchemy.Integer, primary_key=True, autoincrement=True),
+    sqlalchemy.Column("purchase_id", sqlalchemy.Integer, ForeignKey("public_purchase.purchase_id"), nullable=False),
+    sqlalchemy.Column("name_product", sqlalchemy.String(50), nullable=False),
+    sqlalchemy.Column("amount", sqlalchemy.Float, nullable=False),
+    sqlalchemy.Column("price", sqlalchemy.Float, nullable=True),
+    sqlalchemy.Column("quantity", sqlalchemy.Integer, nullable=True)
+)
+
+public_templates = sqlalchemy.Table(
+    "public_templates",
+    metadata,
+    sqlalchemy.Column("template_id", sqlalchemy.Integer, Sequence("public_templates_seq"), server_default=Sequence("public_templates_seq").next_value(), unique=True),
+    sqlalchemy.Column("group_id", sqlalchemy.Integer, ForeignKey("public_cost_group.group_id")),
+    sqlalchemy.Column("name_template", sqlalchemy.String(30)),
+    sqlalchemy.Column("number_days", sqlalchemy.Integer),
+    sqlalchemy.PrimaryKeyConstraint("group_id", "name_template", name="public_templates_pk")
+)
+
+public_templates_category = sqlalchemy.Table(
+    "public_templates_category",
+    metadata,
+    sqlalchemy.Column("template_id", sqlalchemy.Integer, ForeignKey("public_templates.template_id"), nullable=False),
+    sqlalchemy.Column("category_id", sqlalchemy.Integer, ForeignKey("public_category.category_id"), nullable=False),
+    sqlalchemy.PrimaryKeyConstraint("template_id", "category_id", name="public_templates_category_pk")    
+)
